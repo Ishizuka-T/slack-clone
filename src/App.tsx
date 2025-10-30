e635c8f9774d59becda4fac9a9ff711538f5a99d
Ishizuka-T <ishizuka.freesia@gmail.com>
2025-10-30 16:23:10 +0900

    ルーティング設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
+import { BrowserRouter, Routes, Route} from "react-router-dom"
+import Signup from './pages/Signup';
+import Signin from './pages/Signin';
+import CreateWorkspace from './pages/CreateWorkspace';
+import Home from './pages/Home';
+
 function App() {
 
   return (
     <>
-      <div>hello world</div>
+      <BrowserRouter>
+      <div>
+        <Routes>
+          <Route path="signup" element={<Signup />} />
+          <Route path="signin" element={<Signin />} />
+          <Route path="/" element={<CreateWorkspace />} />
+          <Route path="/:workspaceId/:channelId" element={<Home />} />
+        </Routes>
+      </div>
+      </BrowserRouter>
     </>
   )
 }
