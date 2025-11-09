c50b41c9d96696d5bc624e61ba48be319af635a0
Ishizuka-T <ishizuka.freesia@gmail.com>
2025-11-09 23:11:28 +0900

    ワークスペース機能追加改修

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,16 +1,50 @@
-import WorkspaceSelector from './WorkspaceSelector';
-import './Home.css';
-import Sidebar from './Sidebar';
-import MainContent from './MainContent';
+import WorkspaceSelector from "./WorkspaceSelector";
+import "./Home.css";
+import Sidebar from "./Sidebar";
+import MainContent from "./MainContent";
+import { useCurrentUserStore } from "../../modules/auth/current-user.state";
+import { useEffect, useState } from "react";
+import type { Workspace } from "../../modules/workspaces/workspace.entity";
+import { workspaceRepository } from "../../modules/workspaces/workspace.repository";
+import { useParams } from "react-router-dom";
 
 function Home() {
+  const { currentUser } = useCurrentUserStore();
+  const [workspaces, setWorkspaces] = useState<Workspace[]>([]);
+  const params = useParams();
+  const { workspaceId } = params;
+  const selectedWorkspace = workspaces.find(
+    (workspace) => workspace.id == workspaceId
+  );
+
+  useEffect(() => {
+    fetchWorkspaces();
+  }, []);
+
+  const fetchWorkspaces = async () => {
+    try {
+      const workspaces = await workspaceRepository.find();
+      setWorkspaces(workspaces);
+    } catch (error) {
+      console.error(`ワークスペースの作成に失敗しました`, error);
+    }
+  };
+
   return (
     <div className="slack-container">
-      <WorkspaceSelector />
-      <>
-        <Sidebar />
-        <MainContent />
-      </>
+      <WorkspaceSelector
+        workspaces={workspaces}
+        setWorkspaces={setWorkspaces}
+        selectedWorkspaceId={workspaceId}
+      />
+      {selectedWorkspace != null ? (
+        <>
+          <Sidebar selectedWorkspace={selectedWorkspace} />
+          <MainContent />
+        </>
+      ) : (
+        <div className="sidebar" />
+      )}
     </div>
   );
 }
